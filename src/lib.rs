0c832ee3ef2a9273c1714e944c64f4507440ea35
7d <61975820+iiiii7d@users.noreply.github.com>
2026-04-22 01:06:13 +0800

    implement all formatting functions in TextFormat

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,223 @@
-use std::collections::HashMap;
+use std::{borrow::Cow, collections::HashMap};
+
+use itertools::Itertools;
 use proc_macro2::{Ident, Span, TokenStream};
-use quote::{quote, ToTokens, TokenStreamExt};
-use syn::{Expr, FieldValue, LitStr, Token, braced, parse::{Parse, ParseStream}, parse_macro_input, punctuated::Punctuated, token::{Brace, DotDot}, bracketed, parenthesized, ExprLit, parse_quote};
-use syn::parse::End;
-use syn::token::{Bracket, Paren, Token};
+use quote::{ToTokens, TokenStreamExt, quote};
+use syn::{
+    Expr, ExprAssign, ExprLit, ExprPath, FieldValue, Lit, LitByteStr, LitStr, Token, braced,
+    bracketed, parenthesized,
+    parse::{End, Parse, ParseStream},
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    spanned::Spanned,
+    token::{Brace, Bracket, DotDot, Paren, Token},
+};
+
+fn expr2ident(expr: &Expr) -> Option<&Ident> {
+    let Expr::Path(ExprPath { path, .. }) = expr else {
+        return None;
+    };
+    path.segments.iter().exactly_one().ok().map(|a| &a.ident)
+}
 
 #[derive(Clone, Default)]
-struct TextFormat(pub HashMap<Ident, Expr>);
+struct TextFormat(pub HashMap<Ident, TokenStream>);
 impl ToTokens for TextFormat {
     fn to_tokens(&self, token_stream: &mut TokenStream) {
         if self.0.is_empty() {
             token_stream.append_all(quote! { egui::text::TextFormat::default() });
             return;
         }
-        let contents = self.0.iter()
-            .map(|(k, v)| quote!{ #k: #v });
+        let contents = self.0.iter().map(|(k, v)| quote! { #k: #v });
         token_stream.append_all(quote! {
             egui::text::TextFormat {
                 #(#contents),*,
                 ..egui::text::TextFormat::default()
             }
         })
+    }
+}
+impl TextFormat {
+    fn process_font_family(input: &Expr) -> TokenStream {
+        if let Some(input) = expr2ident(input) {
+            match &*input.to_string() {
+                "mono" | "monospace" => quote! { egui::FontFamily::Monospace },
+                "prop" | "proportional" => quote! { egui::FontFamily::Proportional },
+                _ => input.to_token_stream(),
+            }
+        } else if let Expr::Lit(ExprLit {
+            lit: Lit::Str(input),
+            ..
+        }) = input
+        {
+            quote! { egui::FontFamily::Name((#input).into()) }
+        } else {
+            input.to_token_stream()
+        }
+    }
+    fn process_colour(input: &Punctuated<Expr, Token![,]>) -> TokenStream {
+        match input.iter().count() {
+            1 => {
+                let value = input.iter().exactly_one().map_err(|_| ()).unwrap();
+                if let Some(value) = expr2ident(value) {
+                    match &*value.to_string() {
+                        "transparent" => quote! { egui::Color32::TRANSPARENT },
+                        "black" => quote! { egui::Color32::BLACK },
+                        "dark_gray" | "dark_grey" => quote! { egui::Color32::DARK_GRAY },
+                        "gray" | "grey" => quote! { egui::Color32::GRAY },
+                        "light_gray" | "light_grey" => quote! { egui::Color32::LIGHT_GRAY },
+                        "white" => quote! { egui::Color32::WHITE },
+                        "brown" => quote! { egui::Color32::BROWN },
+                        "dark_red" => quote! { egui::Color32::DARK_RED },
+                        "red" => quote! { egui::Color32::RED },
+                        "light_red" => quote! { egui::Color32::LIGHT_RED },
+                        "cyan" => quote! { egui::Color32::CYAN },
+                        "magenta" => quote! { egui::Color32::MAGENTA },
+                        "yellow" => quote! { egui::Color32::YELLOW },
+                        "orange" => quote! { egui::Color32::ORANGE },
+                        "light_yellow" => quote! { egui::Color32::LIGHT_YELLOW },
+                        "khaki" => quote! { egui::Color32::KHAKI },
+                        "dark_green" => quote! { egui::Color32::DARK_GREEN },
+                        "green" => quote! { egui::Color32::GREEN },
+                        "light_green" => quote! { egui::Color32::LIGHT_GREEN },
+                        "dark_blue" => quote! { egui::Color32::DARK_BLUE },
+                        "blue" => quote! { egui::Color32::BLUE },
+                        "light_blue" => quote! { egui::Color32::LIGHT_BLUE },
+                        "purple" => quote! { egui::Color32::PURPLE },
+                        "gold" => quote! { egui::Color32::GOLD },
+                        "debug_color" | "debug" | "debug_colour" => {
+                            quote! { egui::Color32::DEBUG_COLOR }
+                        }
+                        "placeholder" => quote! { egui::Color32::PLACEHOLDER },
+                        _ => value.to_token_stream(),
+                    }
+                } else {
+                    value.to_token_stream()
+                }
+            }
+            3 => {
+                let mut it = input.iter();
+                let r = it.next().expect("");
+                let g = it.next().expect("");
+                let b = it.next().expect("");
+                quote! { egui::Color32::from_rgb(#r, #g, #b) }
+            }
+            4 => {
+                let mut it = input.iter();
+                let r = it.next().expect("");
+                let g = it.next().expect("");
+                let b = it.next().expect("");
+                let a = it.next().expect("");
+                quote! { egui::Color32::from_rgba_unmultiplied(#r, #g, #b, #a) }
+            }
+            _ => panic!(),
+        }
+    }
+    fn process_float(input: &Expr) -> TokenStream {
+        if let Expr::Lit(ExprLit {
+            lit: Lit::Float(value),
+            ..
+        }) = input
+        {
+            quote! { #value }
+        } else if let Expr::Lit(ExprLit {
+            lit: Lit::Int(value),
+            ..
+        }) = input
+        {
+            quote! { #value as f32 }
+        } else {
+            input.to_token_stream()
+        }
+    }
+    fn insert(&mut self, keyword: &Ident, argument: Option<&Punctuated<Expr, Token![,]>>) {
+        let (k, v) = match (&*keyword.to_string(), argument) {
+            ("font_id", Some(value)) => (keyword.clone(), {
+                match value.iter().count() {
+                    1 => value.to_token_stream(),
+                    2 => {
+                        let mut it = value.iter();
+                        let size = Self::process_float(it.next().expect(""));
+                        let family = Self::process_font_family(it.next().expect(""));
+                        quote! { egui::FontId::new(#size, #family) }
+                    }
+                    _ => panic!(),
+                }
+            }),
+            ("extra_letter_spacing" | "expand_bg", Some(value)) => (
+                keyword.clone(),
+                Self::process_float(value.iter().exactly_one().map_err(|_| ()).unwrap()),
+            ),
+            ("line_height", Some(value)) => (keyword.clone(), {
+                let value = value.iter().exactly_one().map_err(|_| ()).unwrap();
+                if let Expr::Lit(ExprLit {
+                    lit: Lit::Float(value),
+                    ..
+                }) = value
+                {
+                    quote! { Some(#value) }
+                } else if let Expr::Lit(ExprLit {
+                    lit: Lit::Int(value),
+                    ..
+                }) = value
+                {
+                    quote! { Some(#value as f32) }
+                } else {
+                    value.to_token_stream()
+                }
+            }),
+            ("color" | "background", Some(value)) => (keyword.clone(), Self::process_colour(value)),
+            ("coords", Some(value)) => (keyword.clone(), {
+                let values = value.iter().map(|kv| {
+                    let Expr::Assign(ExprAssign { left, right, .. }) = kv else {
+                        panic!();
+                    };
+                    let right = Self::process_float(right);
+                    if let Some(left) = expr2ident(left) {
+                        let left = LitByteStr::new(left.to_string().as_bytes(), left.span());
+                        quote! { (#left, #right) }
+                    } else {
+                        quote! { (#left, #right) }
+                    }
+                });
+                quote! { egui::epaint::text::VariationCoords::new([#(#values),*]) }
+            }),
+            ("italics", value) => (
+                keyword.clone(),
+                if let Some(value) = value {
+                    value
+                        .iter()
+                        .exactly_one()
+                        .map_err(|_| ())
+                        .unwrap()
+                        .to_token_stream()
+                } else {
+                    quote!(true)
+                },
+            ),
+            ("underline" | "strikethrough", Some(value)) => (keyword.clone(), {
+                let mut it = value.iter();
+                let width = Self::process_float(it.next().expect(""));
+                let colour = Self::process_colour(&Punctuated::from_iter(it.cloned()));
+                quote! { egui::Stroke::new(#width, #colour) }
+            }),
+            ("valign", Some(value)) => (keyword.clone(), {
+                let value = value.iter().exactly_one().map_err(|_| ()).unwrap();
+                if let Some(value) = expr2ident(value) {
+                    match &*value.to_string() {
+                        "min" => quote! { egui::Align::Min },
+                        "center" | "centre" => quote! { egui::Align::Center },
+                        "max" => quote! { egui::Align::Max },
+                        _ => value.to_token_stream(),
+                    }
+                } else {
+                    value.to_token_stream()
+                }
+            }),
+            (_, None) => panic!(),
+            (_, Some(value)) => (keyword.clone(), quote! {(#value)}),
+        };
+        self.0.insert(k, v);
     }
 }
 
@@ -43,7 +240,8 @@ enum InputSegment {
     Formatting {
         at_tok: Token![@],
         keyword: Ident,
-        argument: Option<(Bracket, Expr)>,
+        brackets: Option<Bracket>,
+        argument: Option<Punctuated<Expr, Token![,]>>,
         parentheses: Paren,
         segments: Vec<InputSegment>,
     },
@@ -54,16 +252,16 @@ impl Parse for InputSegment {
         if input.peek(Token![@]) {
             let at_tok = input.parse()?;
             let keyword = input.parse()?;
-            let argument = if input.peek(Bracket) {
+            let (brackets, argument) = if input.peek(Bracket) {
                 let content;
-                let bracket = bracketed!(content in input);
-                let argument = content.parse::<Expr>()?;
+                let brackets = bracketed!(content in input);
+                let argument = content.parse_terminated(|s| s.parse(), Token![,])?;
                 if !content.peek(End) {
                     return Err(content.error(""));
                 }
-                Some((bracket, argument))
+                (Some(brackets), Some(argument))
             } else {
-                None
+                (None, None)
             };
             let content;
             let parentheses = parenthesized!(content in input);
@@ -74,9 +272,10 @@ impl Parse for InputSegment {
             Ok(Self::Formatting {
                 at_tok,
                 keyword,
+                brackets,
                 argument,
                 parentheses,
-                segments
+                segments,
             })
         } else {
             Ok(Self::Text(input.parse::<Expr>()?))
@@ -86,22 +285,24 @@ impl Parse for InputSegment {
 impl InputSegment {
     fn tokens(&self, token_stream: &mut TokenStream, text_format: &TextFormat) {
         match self {
-            Self::Text(expr) => {
-                token_stream.append_all(quote! {
-                    layout_job.append(
-                        &(#expr).to_string(),
-                        0.0,
-                        #text_format,
-                    );
-                })
-            },
-            Self::Formatting { keyword, argument, segments, .. } => for segment in segments {
-                let mut text_format2 = text_format.to_owned();
-                text_format2.0.insert(keyword.clone(), argument.as_ref().map_or_else(|| {
-                    let expr: Expr = parse_quote!(true);
-                    expr
-                }, |(_, expr)| expr.clone()));
-                segment.tokens(token_stream, &text_format2);
+            Self::Text(expr) => token_stream.append_all(quote! {
+                layout_job.append(
+                    &(#expr).to_string(),
+                    0.0,
+                    #text_format,
+                );
+            }),
+            Self::Formatting {
+                keyword,
+                argument,
+                segments,
+                ..
+            } => {
+                for segment in segments {
+                    let mut text_format2 = text_format.to_owned();
+                    text_format2.insert(keyword, argument.as_ref());
+                    segment.tokens(token_stream, &text_format2);
+                }
             }
         }
     }
@@ -123,15 +324,22 @@ impl Parse for InputArgs {
             segments.push(input.parse()?);
         }
         Ok(Self {
-            input_layout_job, segments
+            input_layout_job,
+            segments,
         })
     }
 }
 
 #[proc_macro]
 pub fn layout_job(token_stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let InputArgs {input_layout_job, segments } = parse_macro_input!(token_stream as InputArgs);
-    let layout_job_tokens = if let Some(InputLayoutJob {input_layout_job, ..}) = input_layout_job {
+    let InputArgs {
+        input_layout_job,
+        segments,
+    } = parse_macro_input!(token_stream as InputArgs);
+    let layout_job_tokens = if let Some(InputLayoutJob {
+        input_layout_job, ..
+    }) = input_layout_job
+    {
         input_layout_job.to_token_stream()
     } else {
         quote! { egui::text::LayoutJob::default() }
@@ -140,10 +348,11 @@ pub fn layout_job(token_stream: proc_macro::TokenStream) -> proc_macro::TokenStr
     let text_format = TextFormat::default();
     for segment in segments {
         segment.tokens(&mut segment_tokens, &text_format);
-    };
+    }
     quote! {{
         let mut layout_job = #layout_job_tokens;
         #segment_tokens
         layout_job
-    }}.into()
+    }}
+    .into()
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,8 @@
-use egui::text::LayoutJob;
-use egui::TextFormat;
+use std::os::unix::raw::dev_t;
+
+use egui::{
+    Align, Color32, FontFamily, Stroke, TextFormat, epaint::text::VariationCoords, text::LayoutJob,
+};
 use egui_layout_job_macro::layout_job;
 
 #[test]
@@ -13,7 +16,7 @@ fn one_literal() {
         let mut l = LayoutJob::default();
         l.append("foobar", 0.0, TextFormat::default());
         l
-    })
+    });
 }
 
 #[test]
@@ -23,7 +26,7 @@ fn two_literals() {
         l.append("foo", 0.0, TextFormat::default());
         l.append("bar", 0.0, TextFormat::default());
         l
-    })
+    });
 }
 
 #[test]
@@ -34,7 +37,7 @@ fn three_literals() {
         l.append("123", 0.0, TextFormat::default());
         l.append("bar", 0.0, TextFormat::default());
         l
-    })
+    });
 }
 
 #[test]
@@ -44,19 +47,22 @@ fn one_ident() {
         let mut l = LayoutJob::default();
         l.append(&variable.to_string(), 0.0, TextFormat::default());
         l
-    })
+    });
 }
 
 #[test]
 fn variety() {
     let variable = 123456;
-    assert_eq!(layout_job!('a' variable format!("{:.2}", std::f32::consts::PI)), {
-        let mut l = LayoutJob::default();
-        l.append("a", 0.0, TextFormat::default());
-        l.append(&variable.to_string(), 0.0, TextFormat::default());
-        l.append("3.14", 0.0, TextFormat::default());
-        l
-    })
+    assert_eq!(
+        layout_job!('a' variable format!("{:.2}", std::f32::consts::PI)),
+        {
+            let mut l = LayoutJob::default();
+            l.append("a", 0.0, TextFormat::default());
+            l.append(&variable.to_string(), 0.0, TextFormat::default());
+            l.append("3.14", 0.0, TextFormat::default());
+            l
+        }
+    );
 }
 
 #[test]
@@ -69,29 +75,363 @@ fn from_existing_layout_job() {
         let mut l = l2;
         l.append("a", 0.0, TextFormat::default());
         l
-    })
+    });
 }
 
 #[test]
-fn format_no_args() {
-    assert_eq!(layout_job!(@italics("a")), {
+fn format_font_id() {
+    assert_eq!(layout_job!(@font_id[10, mono]("a")), {
         let mut l = LayoutJob::default();
-        l.append("a", 0.0, TextFormat {
-            italics: true,
-            ..TextFormat::default()
-        });
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                font_id: egui::FontId::new(10.0, FontFamily::Monospace),
+                ..TextFormat::default()
+            },
+        );
         l
-    })
+    });
+    assert_eq!(layout_job!(@font_id[10.0, prop]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                font_id: egui::FontId::new(10.0, FontFamily::Proportional),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    let size = 5.0;
+    assert_eq!(layout_job!(@font_id[size, "foobar"]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                font_id: egui::FontId::new(size, FontFamily::Name("foobar".into())),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
 }
 
 #[test]
-fn format_with_args() {
+fn format_extra_letter_spacing() {
+    assert_eq!(layout_job!(@extra_letter_spacing[1.0]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                extra_letter_spacing: 1.0,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@extra_letter_spacing[2]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                extra_letter_spacing: 2.0,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_expand_bg() {
     assert_eq!(layout_job!(@expand_bg[1.0]("a")), {
         let mut l = LayoutJob::default();
-        l.append("a", 0.0, TextFormat {
-            expand_bg: 1.0,
-            ..TextFormat::default()
-        });
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                expand_bg: 1.0,
+                ..TextFormat::default()
+            },
+        );
         l
-    })
+    });
+}
+
+#[test]
+fn format_line_height() {
+    assert_eq!(layout_job!(@line_height[1.0]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                line_height: Some(1.0),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@line_height[2]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                line_height: Some(2.0),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@line_height[Some(3.0)]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                line_height: Some(3.0),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_color() {
+    assert_eq!(layout_job!(@color[1, 2, 3]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                color: Color32::from_rgb(1, 2, 3),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@color[1, 2, 3, 4]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                color: Color32::from_rgba_unmultiplied(1, 2, 3, 4),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@color[red]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                color: Color32::RED,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    let color = Color32::RED;
+    assert_eq!(layout_job!(@color[color]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                color,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+#[test]
+fn format_background() {
+    assert_eq!(layout_job!(@background[1, 2, 3]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                background: Color32::from_rgb(1, 2, 3),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_coords() {
+    assert_eq!(layout_job!(@coords[wght=500.0,b"wdth"=75]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                coords: VariationCoords::new([(b"wght", 500.0), (b"wdth", 75.0)]),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_italics() {
+    assert_eq!(layout_job!(@italics("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                italics: true,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@italics[true]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                italics: true,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@italics[false]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                italics: false,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_underline() {
+    assert_eq!(layout_job!(@underline[1.0f32, 1, 2, 3]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                underline: Stroke::new(1.0f32, Color32::from_rgb(1, 2, 3)),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    assert_eq!(layout_job!(@underline[1, red]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                underline: Stroke::new(1.0f32, Color32::RED),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_strikethrough() {
+    assert_eq!(layout_job!(@strikethrough[1.0f32, 1, 2, 3]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                strikethrough: Stroke::new(1.0f32, Color32::from_rgb(1, 2, 3)),
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_valign() {
+    assert_eq!(layout_job!(@valign[min]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                valign: Align::Min,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+    let valign = Align::Center;
+    assert_eq!(layout_job!(@valign[centre]("a")), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                valign: Align::Center,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_nested() {
+    assert_eq!(layout_job!(@color[red](@background[blue]("a"))), {
+        let mut l = LayoutJob::default();
+        l.append(
+            "a",
+            0.0,
+            TextFormat {
+                color: Color32::RED,
+                background: Color32::BLUE,
+                ..TextFormat::default()
+            },
+        );
+        l
+    });
+}
+
+#[test]
+fn format_and_non_format() {
+    assert_eq!(layout_job!("a" @color[red]("b") "c"), {
+        let mut l = LayoutJob::default();
+        l.append("a", 0.0, TextFormat::default());
+        l.append(
+            "b",
+            0.0,
+            TextFormat {
+                color: Color32::RED,
+                ..TextFormat::default()
+            },
+        );
+        l.append("c", 0.0, TextFormat::default());
+
+        l
+    });
 }
